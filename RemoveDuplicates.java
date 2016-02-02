cefa5b8da1da015b4858dbd19ad4a7ab6a7b0e0d
John Wesly <aj_menteng@yahoo.com>
2016-02-02 01:41:44 -0500

    added new files

--- RemoveDuplicates.java
+++ RemoveDuplicates.java
@@ -2,15 +2,30 @@ import java.util.Arrays;
 
 public class RemoveDuplicates{
     public static int[] removeDuplicates(int[] nums) {
+    	/*IF THE ARRAY IS EMPTY OR HAS ONLY ONE ELEMENT,
+    	 * THEN RETURN THE ORIGINAL ARRAY
+    	 */
     	if(nums.length <= 1){
     		return nums;
     	}
+    	
+    	/*USE TWO POINTERS (i) & (j)
+    	 * AND A FLAG (found) TO INFORM IF DUPLICATE IS FOUND
+    	 */
 		int i = 0;
 		boolean found = false;
-    	for(int j = 1; j < nums.length; j++){
+    	
+		/*THE 2ND POINTER (j) IS ALWAYS AHEAD OF (i)
+		 */
+		for(int j = 1; j < nums.length; j++){
     		if(nums[j] == nums[j-1]){
     			found = true;
     		}
+    		/*COMPARE AN ELEMENT WITH THE PREVIOUS ELEMENT
+    		 * IF DIFFERENT AND THE FOUND IS TRUE, THAT MEANS
+    		 * FROM THE PREVIOUS ELEMENTS UNTIL THE 1ST POINTER
+    		 * DUPLICATES.
+    		 */
     		else if(nums[j] != nums[j-1] && found == true){
     			nums[i+1] = nums[j];    
     			i++;
